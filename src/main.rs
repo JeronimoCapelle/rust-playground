12b1f906a350eea2f5947e3157c2483c65c24e7b
InfiniteRideC <jero.capelle@gmail.com>
2026-07-04 13:15:43 -0300

    first basic program

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+fn main() {
+    let mut input_1 = String::default();
+    let mut input_2 = String::default();
+
+    println!("Enter a whole number:");
+    io::stdin().read_line(&mut input_1).expect("error reading");
+
+    println!("Enter another whole number:");
+    io::stdin().read_line(&mut input_2).expect("error reading");
+
+    let num_1: i32 = input_1.trim().parse().expect("not a i32");
+    let num_2: i32 = input_2.trim().parse().expect("not a i32");
+
+    println!("{num_1} + {num_2} is {}", num_1 + num_2);
+    println!("{num_1} - {num_2} is {}", num_1 - num_2);
+}
