ecce21412be8cfaa5b06956aaede00114ba7a37d
InfiniteRideC <jero.capelle@gmail.com>
2026-07-06 14:20:36 -0300

    Simple string manipulation

---
+++ src/bin/08_str_manipulation.rs
@@ -0,0 +1,28 @@
+fn main() {
+    println!("What is your first name?");
+    let first_name = get_input();
+
+    println!("What is your last name?");
+    let last_name = get_input();
+
+    let name = build_name(&first_name, &last_name);
+
+    println!("Your full name is:");
+    println!("{name}");
+}
+
+fn build_name(first_name: &str, last_name: &str) -> String {
+    let mut name = String::default();
+    name.push_str(first_name);
+    name.push(' ');
+    name.push_str(last_name);
+    name
+}
+
+fn get_input() -> String {
+    let mut input = String::default();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read");
+    input.trim().to_string()
+}
