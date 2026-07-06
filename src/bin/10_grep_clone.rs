6108c01b863e352ab688ccb9f44002722ea215ca
InfiniteRideC <jero.capelle@gmail.com>
2026-07-06 18:17:49 -0300

    implemented args

--- src/bin/10_grep_clone.rs
+++ src/bin/10_grep_clone.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let pattern = "Hello";
-    let contents = std::fs::read_to_string("input.txt").expect("failed to open");
+    let inputs: Vec<_> = std::env::args().collect();
+    let contents = std::fs::read_to_string(&inputs[1]).expect("failed to open");
 
     for i in contents.split('\n') {
-        if i.contains(pattern) {
+        if i.contains(&inputs[2]) {
             println!("{i}");
         }
     }
