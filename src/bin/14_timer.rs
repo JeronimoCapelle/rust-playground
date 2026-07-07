242a3cbec09b4651c66ce2459a13704cb6f8bc5a
InfiniteRideC <jero.capelle@gmail.com>
2026-07-07 19:38:19 -0300

    Timer CLI

--- src/bin/14_timer.rs
+++ src/bin/14_timer.rs
@@ -1,12 +1,18 @@
-use std::time::Duration;
+const LOADING_BAR_SIZE: usize = 20;
+const SECONDS_PER_MINUTE: usize = 60;
+const FILLED_BAR: char = '#';
 
 fn main() {
-    let initial_time = get_input();
+    let initial_time: usize = std::env::args()
+        .nth(1)
+        .expect("The first arg should be time in seconds")
+        .parse()
+        .expect("Not a number");
 
     for i in 0..=initial_time {
         clear_terminal();
-        display_number(initial_time - i);
-        std::thread::sleep(Duration::from_secs(1));
+        display_time(initial_time, initial_time - i);
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
 
@@ -14,26 +20,19 @@ fn clear_terminal() {
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 }
 
-fn get_input() -> i32 {
-    let mut string = String::default();
-    std::io::stdin()
-        .read_line(&mut string)
-        .expect("Error reading");
-    string.trim().parse().expect("Not a number")
-}
+fn display_time(initial_time: usize, time: usize) {
+    let mut bar = String::new();
 
-fn display_number(time: i32) {
-    println!(",-------------------,");
-    println!("|      +-----+      |");
-    println!("|      |timer|      |");
-    println!("|      +-----+      |");
-    println!("|nununununununununun|");
-    println!("|     ,-------.     |");
-    println!("|  ,-'         `-.  |");
-    println!("| /               \\ |");
-    println!("|(       {time:^3}       )|");
-    println!("| \\               / |");
-    println!("|  `-.         ,-'  |");
-    println!("|     `-------'     |");
-    println!("'-------------------'");
+    let size = (initial_time - time) * LOADING_BAR_SIZE / initial_time;
+    for _ in 0..size {
+        bar.push(FILLED_BAR);
+    }
+
+    dbg!(&bar);
+
+    println!(
+        "[{bar:<LOADING_BAR_SIZE$}] {:02}:{:02}",
+        time / SECONDS_PER_MINUTE,
+        time % SECONDS_PER_MINUTE
+    );
 }
